4be19d468367696a82759396529004692d3c1c7f
tareq3 <mti.tareq3@gmail.com>
2019-09-14 15:58:24 +0600

    remove button added

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ let nameField = document.querySelector("#nameField");
 
 nameField.addEventListener("keypress", function(event) {
   if (event.keyCode === 13) {
-    uList.appendChild(createLi(event.target.value));
+    createLi(uList, event.target.value);
+    event.target.value = "";
   }
 });
 
-function createLi(data) {
+function createLi(uList, data) {
   let lItem = document.createElement("li");
-  lItem.className = "list-group-item";
+  lItem.className = "list-group-item my-2 d-flex text-primary";
   lItem.innerHTML = data;
-  return lItem;
+
+  let mySpan = document.createElement("span");
+  mySpan.innerHTML = "X";
+  mySpan.className = "ml-auto";
+  mySpan.style.color = "red";
+  mySpan.style.cursor = "pointer";
+
+  mySpan.addEventListener("click", function() {
+    uList.removeChild(lItem);
+  });
+
+  lItem.appendChild(mySpan);
+
+  console.log(mySpan);
+  uList.appendChild(lItem);
 }
